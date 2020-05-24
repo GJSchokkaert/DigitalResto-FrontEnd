2c235a119629e0eec58d06561941d4e8217a38ba
Gert-Jan Schokkaert <gert-jan.schokkaert@ordina.be>
2020-05-24 18:50:30 +0200

    added deploy to netlify

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-import Bar from "./routes/Bar.svelte";
-import Waiter from "./routes/Waiter.svelte";
-import NotFound from "./routes/NotFound.svelte";
+import Bar from "./Views/Bar.svelte";
+import Waiter from "./Views/Waiter.svelte";
+import NotFound from "./Views/NotFound.svelte";
 
 const routes = {
   // Index page for now just Bar
